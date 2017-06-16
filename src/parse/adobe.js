ce1e732eb5c3e841fa29f2224f8474cfbcb36a6b
Olve <oisevb@gmail.com>
2017-06-16 14:12:13 +0200

    add parseAdobe

--- src/parse/adobe.js
+++ src/parse/adobe.js
@@ -1,3 +1,16 @@
-export default function parseAdobe() {
-  
+import readAdobe from '../read/adobe'
+import {readLength} from '../read/app1'
+
+export default function parseAdobe(segment) {
+  const { xml } = readAdobe(segment.offset, segment._buffer)
+
+  return {
+    value: xml,
+    bytes: function() {
+      const array = new Uint8Array(segment._buffer)
+      const end = segment.offset + 32 + readLength(segment.offset, segment._buffer)
+      return Array.from(array.slice(segment.offset, end))
+    }
+  }
+
 }
