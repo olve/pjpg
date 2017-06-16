5c19ad8980c42f24ce36974280f25ed7816db18f
Olve <oisevb@gmail.com>
2017-06-16 14:34:46 +0200

    wip parse-exif

--- src/parse/exif.js
+++ src/parse/exif.js
@@ -1,3 +1,11 @@
-export default function parseExif() {
+import {readExif} from '../read/exif'
+
+export default function parseExif(segment) {
+  const exif = readExif(segment.offset, segment._buffer)
+
+  return {
+    value: exif,
+    bytes: [],
+  }
   
 }
