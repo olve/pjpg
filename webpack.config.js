e6a2d2a0269dbc461ee4182ed94f10678d2b352c
Olve <oisevb@gmail.com>
2017-06-16 13:03:16 +0200

    add web pack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'index.js',
+        library: 'pjpg',
+        libraryTarget: 'umd',
+    },
+
+    module: {
+      // use `test` to split a single file
+      // or `include` to split a whole folder
+      rules: [
+        { test: /\.jsx?$/,
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: [
+              ['env', {
+                modules: false,
+                targets: {
+                  'browsers': ['last 2 versions']
+                }
+              }],
+              'stage-0',
+            ],
+            plugins: [
+              'transform-runtime',
+            ]
+          },
+          exclude: /node_modules/
+        },
+      ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    plugins: [
+        new webpack.SourceMapDevToolPlugin(),
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          beautify: false,
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          },
+          compress: {
+            screw_ie8: true
+          },
+          comments: false
+        }),
+     ],
+}
