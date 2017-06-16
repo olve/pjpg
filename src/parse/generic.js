6f2abd96b5ea1a8c549ca7dff47e64c7e958c110
Olve <oisevb@gmail.com>
2017-06-16 14:14:50 +0200

    add parseGeneric

--- src/parse/generic.js
+++ src/parse/generic.js
@@ -1,3 +1,12 @@
-export default function parseGeneric() {
-  
+import readGeneric from '../read/generic'
+
+export default function parseGeneric(segment) {
+
+  return {
+    value: null,
+    bytes: function() {
+      return readGeneric(segment.offset, segment._buffer)
+    }
+  }
+
 }
