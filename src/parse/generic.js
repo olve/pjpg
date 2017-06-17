c94ee75e7adfd0e20f2ceaf4490a4c0ec69406ab
Olve <oisevb@gmail.com>
2017-06-17 13:03:01 +0200

    fix args

--- src/parse/generic.js
+++ src/parse/generic.js
@@ -5,7 +5,7 @@ export default function parseGeneric(segment) {
   return {
     value: null,
     bytes: function() {
-      return readGeneric(segment.offset, segment._buffer)
+      return readGeneric(segment.marker.offset, segment.jpeg)
     }
   }
 
