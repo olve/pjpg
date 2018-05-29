623f123217b8e5851154c6ea642da7490af34713
Olve <oisevb@gmail.com>
2018-05-29 15:20:26 +0200

    explain why data is removed from exif

--- src/parse/exif.js
+++ src/parse/exif.js
@@ -3,7 +3,11 @@ import { exifToBytes } from '../write/exif'
 import { readLength } from '../read/app1'
 
 export default function parseExif(segment) {
+
   const exif = readExif(segment.marker, segment.jpeg)
+
+
+  //remove markers found within exif segment; they are from embedded images.
   const length = readLength(segment.marker.offset, segment.jpeg)
   const { index, offset } = segment.marker
   segment.jpeg.spliceMarkers(index, offset, offset+length+2)
